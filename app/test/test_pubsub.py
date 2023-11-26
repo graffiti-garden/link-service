ab0f09c9a478e451cf5ffd75202c351762627dff
theia <theia@mit.edu>
2023-11-26 16:10:10 -0500

    pubsub connection made simpler in tests

--- app/test/test_pubsub.py
+++ app/test/test_pubsub.py
@@ -5,113 +5,106 @@ import aiohttp
 from ..pubsub import msg_header_format, RequestHeader, ResponseHeader
 import struct
 from random import randbytes
-
-URL_BASE = 'http://localhost:8000/'
+from .utils import socket_connection
 
 class TestPubSub(unittest.IsolatedAsyncioTestCase):
 
     async def test_invalid_header(self):
-        async with aiohttp.ClientSession() as session:
-            for num_bytes in [0, 5, struct.calcsize(msg_header_format) - 1]:
-                async with session.ws_connect(URL_BASE) as ws:
-                    await ws.send_bytes(randbytes(num_bytes))
-                    reply = await ws.receive() 
-                    self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
-                    self.assertEqual(reply.data, ResponseHeader.ERROR_WITHOUT_ID.value + b'not enough data')
-
-                    reply = await ws.receive()
-                    self.assertEqual(reply.type, aiohttp.WSMsgType.CLOSE)
-
-    async def test_no_info_hash(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(URL_BASE) as ws:
-                for request in [RequestHeader.SUBSCRIBE, RequestHeader.UNSUBSCRIBE]:
-                    message_id = randbytes(16)
-                    await ws.send_bytes(struct.pack(
-                        msg_header_format,
-                        0,
-                        request.value,
-                        message_id
-                    ))
-
-                    reply = await ws.receive() 
-                    self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
-                    self.assertEqual(reply.data,
-                        ResponseHeader.ERROR_WITH_ID.value +
-                        message_id +
-                        b'no info hash'
-                    )
-
-    async def test_wrong_version(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(URL_BASE) as ws:
-                message_id = randbytes(16)
-                await ws.send_bytes(struct.pack(
-                    msg_header_format,
-                    1,
-                    RequestHeader.UNSUBSCRIBE.value,
-                    message_id
-                 ))
-
+        for num_bytes in [0, 5, struct.calcsize(msg_header_format) - 1]:
+            async with socket_connection() as ws:
+                await ws.send_bytes(randbytes(num_bytes))
                 reply = await ws.receive() 
                 self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
-                self.assertEqual(reply.data,
-                    ResponseHeader.ERROR_WITH_ID.value +
-                    message_id +
-                    b'this is version zero'
-                )
+                self.assertEqual(reply.data, ResponseHeader.ERROR_WITHOUT_ID.value + b'not enough data')
 
-    async def test_non_existant_request(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(URL_BASE) as ws:
+                reply = await ws.receive()
+                self.assertEqual(reply.type, aiohttp.WSMsgType.CLOSE)
+
+    async def test_no_info_hash(self):
+        async with socket_connection() as ws:
+            for request in [RequestHeader.SUBSCRIBE, RequestHeader.UNSUBSCRIBE]:
                 message_id = randbytes(16)
                 await ws.send_bytes(struct.pack(
                     msg_header_format,
                     0,
-                    15,
+                    request.value,
                     message_id
-                 ) + randbytes(32))
+                ))
 
                 reply = await ws.receive() 
                 self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
                 self.assertEqual(reply.data,
                     ResponseHeader.ERROR_WITH_ID.value +
                     message_id +
-                    b'invalid request'
+                    b'no info hash'
                 )
 
-    async def test_info_hash(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(URL_BASE) as ws:
-                for test_good in [True, False]:
-                    for request in [RequestHeader.SUBSCRIBE, RequestHeader.UNSUBSCRIBE]:
-                        for byte_nums in [32, 64, 128, 32*32] if test_good else [1, 5, 12, 24, 37, 1000]:
-                            message_id = randbytes(16)
-                            await ws.send_bytes(struct.pack(
-                                msg_header_format,
-                                0,
-                                request.value,
-                                message_id
-                            ) + randbytes(byte_nums))
+    async def test_wrong_version(self):
+        async with socket_connection() as ws:
+            message_id = randbytes(16)
+            await ws.send_bytes(struct.pack(
+                msg_header_format,
+                1,
+                RequestHeader.UNSUBSCRIBE.value,
+                message_id
+                ))
 
-                            reply = await ws.receive()
-                            if test_good:
-                                self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
-                                self.assertEqual(reply.data, ResponseHeader.SUCCESS.value + message_id)
-                            else:
-                                self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
-                                self.assertEqual(reply.data,
-                                    ResponseHeader.ERROR_WITH_ID.value +
-                                    message_id +
-                                    b'info hashes must each be exactly 32 bytes'
-                                )
+            reply = await ws.receive() 
+            self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
+            self.assertEqual(reply.data,
+                ResponseHeader.ERROR_WITH_ID.value +
+                message_id +
+                b'this is version zero'
+            )
+
+    async def test_non_existant_request(self):
+        async with socket_connection() as ws:
+            message_id = randbytes(16)
+            await ws.send_bytes(struct.pack(
+                msg_header_format,
+                0,
+                15,
+                message_id
+                ) + randbytes(32))
+
+            reply = await ws.receive() 
+            self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
+            self.assertEqual(reply.data,
+                ResponseHeader.ERROR_WITH_ID.value +
+                message_id +
+                b'invalid request'
+            )
+
+    async def test_info_hash(self):
+        async with socket_connection() as ws:
+            for test_good in [True, False]:
+                for request in [RequestHeader.SUBSCRIBE, RequestHeader.UNSUBSCRIBE]:
+                    for byte_nums in [32, 64, 128, 32*32] if test_good else [1, 5, 12, 24, 37, 1000]:
+                        message_id = randbytes(16)
+                        await ws.send_bytes(struct.pack(
+                            msg_header_format,
+                            0,
+                            request.value,
+                            message_id
+                        ) + randbytes(byte_nums))
+
+                        reply = await ws.receive()
+                        if test_good:
+                            self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
+                            self.assertEqual(reply.data, ResponseHeader.SUCCESS.value + message_id)
+                        else:
+                            self.assertEqual(reply.type, aiohttp.WSMsgType.BINARY)
+                            self.assertEqual(reply.data,
+                                ResponseHeader.ERROR_WITH_ID.value +
+                                message_id +
+                                b'info hashes must each be exactly 32 bytes'
+                            )
 
     async def test_close(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(URL_BASE) as ws:
-                await ws.close()
-                reply = await ws.receive() 
-                self.assertEqual(reply.type, aiohttp.WSMsgType.CLOSED)
+        async with socket_connection() as ws:
+            await ws.close()
+            reply = await ws.receive() 
+            self.assertEqual(reply.type, aiohttp.WSMsgType.CLOSED)
 
 if __name__ == "__main__":
     unittest.main()
--- app/test/utils.py
+++ app/test/utils.py
@@ -3,16 +3,17 @@ import aiohttp
 import base64
 from ..rest import put_metadata_format
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from contextlib import asynccontextmanager
 
 URL_BASE = 'http://localhost:8000/'
 
-def editor_public_private_keys():
-    private_key = Ed25519PrivateKey.generate()
+def editor_public_private_keys(private_key=None):
+    private_key = private_key if private_key else Ed25519PrivateKey.generate()
     public_key = private_key.public_key().public_bytes_raw()
     return public_key, private_key
 
-def generate_info_hash_and_pok(editor_public_key):
-    uri_private_key = Ed25519PrivateKey.generate()
+def generate_info_hash_and_pok(editor_public_key, uri_private_key=None):
+    uri_private_key = uri_private_key if uri_private_key else Ed25519PrivateKey.generate()
     info_hash = uri_private_key.public_key().public_bytes_raw()
     pok = uri_private_key.sign(editor_public_key)
     return info_hash, pok
@@ -50,31 +51,40 @@ async def put(
 
             return url, container_signed, response.status, await response.read()
 
-# async def put_link(
-#     source,
-#     payload,
-#     expiration,
-#     counter,
-#     private_key=Ed25519PrivateKey.generate()
-# ):
-#     public_key, private_key = editor_public_private_keys()
+"""
+Put a random link containing a random
+payload at the given uri, with the
+given editor key
+"""
+async def put_random_link(
+    expiration,
+    counter,
+    payload=None,
+    editor_private_key=None,
+    uri_private_key=None
+):
+    payload = payload if payload else randbytes(16)
+    editor_public_key, _ = editor_public_private_keys(editor_private_key)
+    info_hash, pok = generate_info_hash_and_pok(editor_public_key, uri_private_key)
 
-#     uri_private_key = Ed25519PrivateKey.generate()
-#     info_hash = uri_private_key.public_key().public_bytes_raw()
-#     pok = uri_private_key.sign(editor_public_key)
+    await put(
+        editor_public_key,
+        editor_private_key,
+        0,
+        info_hash,
+        pok,
+        counter,
+        expiration,
+        payload
+    )
 
-#     await put(
-#         public_key,
-#         private_key,
-#         0,
-#         info_hash,
-#         pok,
-#         counter,
-#         expiration,
-#         payload
-#     )
+    return editor_public_key, editor_private_key, info_hash, uri_private_key, payload
 
-#     return private_key
+@asynccontextmanager
+async def socket_connection():
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(URL_BASE) as ws:
+            yield ws
 
 # async def unpack_container(msg):
 #     return editor_public_key, source, payload, expiration, counter
